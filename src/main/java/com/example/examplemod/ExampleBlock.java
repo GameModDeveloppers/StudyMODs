877def3139a67aaeaff7ac8a82c6745017c4c3b2
t-kono <see_you_space_cowboy@hotmail.co.jp>
2016-04-24 19:40:45 +0900

    サンプルブロック追加(v1.8)

---
+++ src/main/java/com/example/examplemod/ExampleBlock.java
@@ -0,0 +1,63 @@
+package com.example.examplemod;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+import net.minecraft.creativetab.CreativeTabs;
+
+//
+//	ExampleBlockクラス
+//	Blockクラス継承
+//
+
+public class ExampleBlock extends Block
+{
+	// MOD管理名：システムへの登録時に使用
+	public static final String MOD_ENTRY_NAME = "exampleblock";
+
+
+	// ブロックの元にするマテリアル情報
+	private static final Material BASE_MATERIAL = Material.rock;				// 石ブロック
+	// このブロックが表示されるクリエイティヴモードのタブ
+	private static final CreativeTabs BLOCK_SHOW_TAB = CreativeTabs.tabBlock;		// ブロック系タブ
+
+	// ブロックの硬さ
+	private static final float BLOCK_HARDNESS = 1.0f;
+	// ブロックの爆破耐性
+	private static final float BLOCK_RESISTANCE = 1.0f;
+	// ブロックを歩いた時の音
+	private static final SoundType BLOCK_STEPSOUND = soundTypeStone;
+
+	// ブロック透過係数(デフォルト=0)
+	private static final int BLOCK_OPACITY = 1;
+	// ブロックの明るさ(15 = 1.0f)
+	private static final float BLOCK_BRIGHTLEVEL = 1.0f;
+
+	//
+	//	コンストラクタ
+	//
+	public ExampleBlock()
+	{
+		// 継承元クラスのコンストラクタコール
+		super(BASE_MATERIAL);
+
+		// クリエイティブモードで表示されるタブの選択
+		setCreativeTab(BLOCK_SHOW_TAB);
+		setUnlocalizedName("BlockExample");
+
+		//
+		//	ブロックパラメータの設定
+		//
+		setHardness(BLOCK_HARDNESS);				// 硬さ
+		setResistance(BLOCK_RESISTANCE);			// 爆破耐性
+		setStepSound(BLOCK_STEPSOUND);			// 歩行音
+		//setBlockUnbreakable();					// 破壊可能フラグ
+		//setTickRandomly(true);					// ブロックのTick()処理をランダムにコール
+		//disableStats();							// ブロックの統計情報を保存しない
+		setLightOpacity(BLOCK_OPACITY);			// ブロック透過係数
+		setLightLevel(BLOCK_BRIGHTLEVEL);			// ブロック明るさ
+
+		// 初期ブロック状態の取得
+		//this.setDefaultState(getBlockState().getBaseState());
+	}
+}
+
--- src/main/java/com/example/examplemod/ExampleMod.java
+++ src/main/java/com/example/examplemod/ExampleMod.java
@@ -1,20 +1,52 @@
 package com.example.examplemod;
 
-import net.minecraft.init.Blocks;
+import net.minecraft.block.Block;
+import net.minecraft.client.resources.model.ModelResourceLocation;
+import net.minecraft.item.Item;
+import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
-import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.registry.GameRegistry;
 
-@Mod(modid = ExampleMod.MODID, version = ExampleMod.VERSION)
+@Mod(modid = ExampleMod.MOD_ID,
+	 name = ExampleMod.MOD_NAME,
+	 version = ExampleMod.MOD_VERSION,
+	 dependencies = ExampleMod.MOD_DEPENDENCIES,
+	 acceptedMinecraftVersions = ExampleMod.MOD_ACCEPTED_MC_VERSIONS,
+	 useMetadata = true)		// metadata使用
 public class ExampleMod
 {
-    public static final String MODID = "examplemod";
-    public static final String VERSION = "1.0";
-    
-    @EventHandler
-    public void init(FMLInitializationEvent event)
-    {
-		// some example code
-        System.out.println("DIRT BLOCK >> "+Blocks.dirt.getUnlocalizedName());
-    }
+	// MOD-ID文字列
+	public static final String MOD_ID = "examplemod";
+	// MOD名称
+	public static final String MOD_NAME = "ExampleMod";
+	// MODバージョン
+	public static final String MOD_VERSION = "1.0";
+	// 前提MOD(少なくとも開発に使用するForgeのバージョンを入れる)
+	public static final String MOD_DEPENDENCIES = "required-after:Forge@[1.8.9-11.15.0.1705,)";
+	// 起動できるMinecraft本体バージョン
+	// 記法はMavenのVersion Range Specificationを検索すること
+	public static final String MOD_ACCEPTED_MC_VERSIONS = "[1.8, 1.8.9]";
+
+	// 追加したいブロックのインスタンス格納先
+	// レシピ等で利用する
+	public static Block addBlock;
+
+	@EventHandler
+	public void preInit(FMLPreInitializationEvent event)
+	{
+		// 追加するブロックインスタンス作成
+		addBlock = new ExampleBlock();
+
+		// ゲームレジストリに登録
+		GameRegistry.registerBlock(addBlock, ExampleBlock.MOD_ENTRY_NAME);
+
+		// テクスチャモデル指定JSONファイル名の登録(クライアント起動の場合)
+		if (true == event.getSide().isClient()) {
+			// モデル情報登録
+			ModelLoader.setCustomModelResourceLocation(Item.getItemFromBlock(addBlock), 0,
+															new ModelResourceLocation(MOD_ID + ":" + ExampleBlock.MOD_ENTRY_NAME, "inventory"));
+		}
+	}
 }
